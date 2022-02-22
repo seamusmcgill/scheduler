548c6fd9c379d02955ca094c3c435f1d9564dacd
Seamus McGill <seamusmcgill@gmail.com>
2022-02-21 20:08:10 -0500

    Add interactivity and pass props to button

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,15 +1,24 @@
 import React from "react";
+import classNames from "classnames";
 
 import "components/Button.scss";
 
 export default function Button(props) {
+
   const { confirm, danger, onClick, disabled } = props
   
   let buttonClass = classNames('button', {
     'button--confirm' : confirm, 
     'button--danger': danger
   });
+
+   return (
+    <button 
+     className={buttonClass} 
+     onClick={onClick} 
+     disabled={disabled} 
     >
        {props.children}
     </button>
+   );
 }
