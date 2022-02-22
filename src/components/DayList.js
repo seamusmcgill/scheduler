65b83f8f57f772829dd8f608e9ae61e410e9ea53
Seamus McGill <seamusmcgill@gmail.com>
2022-02-21 20:12:20 -0500

    Create dayList and pass data dyncamically

---
+++ src/components/DayList.js
@@ -0,0 +1,16 @@
+import React from "react";
+import DayListItem from "./DayListItem";
+
+export default function DayList(props) {
+  const { days, day, setDay } = props
+
+  const daysArray = days.map(day => { 
+    return <DayListItem key={day.id} name={day.name} spots={day.spots} selected={day.name === props.day} setDay={setDay} /> 
+  });
+  
+  return (
+    <ul>
+      {daysArray}
+    </ul>
+  )
+}
