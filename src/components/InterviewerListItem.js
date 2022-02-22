57cd382c4627ee14debf05b0954e8386180457d9
Seamus McGill <seamusmcgill@gmail.com>
2022-02-21 20:46:07 -0500

    Add conditional styling and pass props into component

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import classNames from 'classnames';
+import "./InterviewerListItem.scss"
 
 export default function InterviewerListItem(props) {
   const { id, name, avatar, selected, setInterviewer } = props;
 
+  const interviewersClass = classNames('interviewers__item', {
+    'interviewers__item--selected' : selected
+  })
+
   return (
-    <li className="interviewers__item">
+    <li className={interviewersClass} onClick={() => setInterviewer(id)} >
       <img
-        className="interviewers__item-image"
-        src="https://i.imgur.com/LpaY82x.png"
-        alt="Sylvia Palmer"
+        className={`interviewers__item-image`}
+        src={avatar}
+        alt={name}
       />
-      Sylvia Palmer
+      {selected ? name : ''}
     </li>
   )
 }
