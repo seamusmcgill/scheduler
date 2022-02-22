a36324205ce53edc798a4e63375cacad0ce30995
Seamus McGill <seamusmcgill@gmail.com>
2022-02-21 20:07:31 -0500

    Add conditional styling classes

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,6 +3,12 @@ import React from "react";
 import "components/Button.scss";
 
 export default function Button(props) {
+  const { confirm, danger, onClick, disabled } = props
+  
+  let buttonClass = classNames('button', {
+    'button--confirm' : confirm, 
+    'button--danger': danger
+  });
     >
        {props.children}
     </button>
