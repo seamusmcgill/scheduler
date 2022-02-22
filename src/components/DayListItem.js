8461f15b8e381811dd7786dc776da9382675c7fd
Seamus McGill <seamusmcgill@gmail.com>
2022-02-21 20:10:00 -0500

    Add props and click handler to component

---
+++ src/components/DayListItem.js
@@ -0,0 +1,22 @@
+import React from "react";
+import classNames from 'classnames'
+import "./DayListItem.scss"
+
+export default function DayListItem(props) {
+
+  const { name, spots, selected, setDay } = props
+  const dayClass = classNames('day-list__item', {
+    'day-list__item--selected': selected,
+    'day-list__item--full': spots === 0
+  })
+  const formatSpots = spots === 0 ? 'no spots remaining' : (spots === 1 ? '1 spot remaining' : `${spots} spots remaining`)
+
+  return (
+    <li className={dayClass} onClick={() => setDay(name)} selected={selected}>
+      <h2 className="text--regular">{name}</h2> 
+      <h3 className="text--light">
+        {formatSpots}
+      </h3>
+    </li>
+  );
+}
