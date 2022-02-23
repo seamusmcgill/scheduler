a234cf5c1c31f2046ece382956aa9dc87d8d2358
Seamus McGill <seamusmcgill@gmail.com>
2022-02-23 10:01:32 -0500

    Remove hardcoded days, fetch from server instead

--- src/components/Application.js
+++ src/components/Application.js
@@ -4,24 +4,6 @@ import DayList from "./DayList";
 import "components/Application.scss";
 import Appointment from "./Appointment";
 
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
-
 const appointments = [
   {
     id: 1,
@@ -63,6 +45,12 @@ const appointments = [
 
 export default function Application(props) {
   const [ day, setDay ] = useState('Monday')
+  const [ days, setDays ] = useState([])
+
+  useEffect(() => {
+    axios.get('/api/days')
+      .then(response => setDays(response.data))
+  }, [])
 
   return (
     <main className="layout">
