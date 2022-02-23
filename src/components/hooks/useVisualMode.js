2a38683f5d49d6b555ca3baeea82d40a453067b3
Seamus McGill <seamusmcgill@gmail.com>
2022-02-23 17:12:05 -0500

    Create useVisualMode hook

---
+++ src/components/hooks/useVisualMode.js
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+
+export default function useVisualMode(initial) {
+  const [ mode, setMode ] = useState(initial);
+  const [ history, setHistory ] = useState([initial])
+
+  const transition = (mode, replace = false) => { 
+    if (replace) {
+      history.pop()
+    }
+    history.push(mode)
+    setMode(mode) 
+  } 
+
+  const back = () => { 
+    if (history.length === 1) {
+      return
+    }
+    history.pop() 
+    setMode(history[history.length - 1])
+  };
+
+  return { mode, transition, back }
+}
