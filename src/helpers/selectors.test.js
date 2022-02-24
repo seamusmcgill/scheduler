2392ce7ddbbf01e5df7d6b1658bfb8041afb7423
Seamus McGill <seamusmcgill@gmail.com>
2022-02-23 19:45:09 -0500

    Update state.days.interviewers to be array of IDs

--- src/helpers/selectors.test.js
+++ src/helpers/selectors.test.js
@@ -6,18 +6,7 @@ const state = {
       id: 1,
       name: "Monday",
       appointments: [1, 2, 3],
-      interviewers: [
-        {  
-          "id": 1,
-          "name": "Sylvia Palmer",
-          "avatar": "https://i.imgur.com/LpaY82x.png"
-        },
-        {
-          id: 2,
-          name: "Tori Malcolm",
-          avatar: "https://i.imgur.com/Nmx0Qxo.png"
-        }
-      ]
+      interviewers: [1, 2]
     },
     {
       id: 2,
