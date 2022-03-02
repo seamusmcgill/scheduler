cec46829c4174e8783226632c93c20cdb9f41a72
Seamus McGill <seamusmcgill@gmail.com>
2022-03-01 21:03:19 -0500

    Format style and add data-testid to list item

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,22 +1,24 @@
 import React from "react";
-import classNames from 'classnames'
-import "./DayListItem.scss"
+import classNames from "classnames";
+import "./DayListItem.scss";
 
 export default function DayListItem(props) {
-
-  const { name, spots, selected, setDay } = props
-  const dayClass = classNames('day-list__item', {
-    'day-list__item--selected': selected,
-    'day-list__item--full': spots === 0
-  })
-  const formatSpots = spots === 0 ? 'no spots remaining' : (spots === 1 ? '1 spot remaining' : `${spots} spots remaining`)
+  const { name, spots, selected, setDay } = props;
+  const dayClass = classNames("day-list__item", {
+    "day-list__item--selected": selected,
+    "day-list__item--full": spots === 0,
+  });
+  const formatSpots =
+    spots === 0
+      ? "no spots remaining"
+      : spots === 1
+      ? "1 spot remaining"
+      : `${spots} spots remaining`;
 
   return (
-    <li className={dayClass} onClick={() => setDay(name)} selected={selected}>
-      <h2 className="text--regular">{name}</h2> 
-      <h3 className="text--light">
-        {formatSpots}
-      </h3>
+    <li className={dayClass} onClick={() => setDay(name)} selected={selected} data-testid="day">
+      <h2 className="text--regular">{name}</h2>
+      <h3 className="text--light">{formatSpots}</h3>
     </li>
   );
 }
